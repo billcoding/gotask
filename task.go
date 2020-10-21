6e1a1c2b0415e70ed50cc07b4178728866fa7242
wangmiao <wangmiao07@sina.com>
2020-10-21 12:05:38 +0800

    Add stop() for worker

--- task.go
+++ task.go
@@ -5,6 +5,7 @@ import (
 	"log"
 )
 
+//Define task interface
 type Task interface {
 	Name() string
 	Scheduled() string
@@ -14,6 +15,7 @@ type Task interface {
 var c = cron.New()
 var close = make(chan bool, 1)
 
+//Start task worker
 func Start() {
 	defer func() {
 		if re := recover(); re != nil {
@@ -25,10 +27,17 @@ func Start() {
 	log.Println("gotask service started")
 }
 
+//Await task worker
 func Wait() {
 	<-close
 }
 
+//Stop task worker
+func Stop() {
+	close <- true
+}
+
+//Add a task
 func Add(task Task) {
 	c.AddFunc(task.Scheduled(), task.Run())
 	log.Printf("gotask add task[name:%s, scheduled:%s]\n", task.Name(), task.Scheduled())
--- task_test.go
+++ task_test.go
@@ -5,23 +5,28 @@ import (
 	"testing"
 )
 
+//Define printTask struct
 type printTask struct {
 }
 
+//Implements Task Name method
 func (printTask) Name() string {
 	return "printTask"
 }
 
+//Implements Task Scheduled method
 func (printTask) Scheduled() string {
 	return "@every 1s"
 }
 
+//Implements Task Run method
 func (printTask) Run() func() {
 	return func() {
 		fmt.Println("helloworld")
 	}
 }
 
+//Test Task
 func TestTask(t *testing.T) {
 	Add(printTask{})
 	Start()
